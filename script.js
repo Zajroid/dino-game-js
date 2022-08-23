16ec5c35e59f88a5514ccf1aab28c2dfbddbb258
Danya <zajroid@gmail.com>
2022-08-23 15:29:37 +0300

    add lose logic game

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-import { setupGround, updateGround } from "./ground.js";
-import { updateDino, setupDino } from "./dino.js";
-import { updateCactus, setupCactus } from "./cactus.js";
+import { updateGround, setupGround } from "./ground.js";
+import { updateDino, setupDino, getDinoRect, setDinoLose } from "./dino.js";
+import { updateCactus, setupCactus, getCactusRects } from "./cactus.js";
 
 const WORLD_WIDTH = 100;
 const WORLD_HEIGHT = 30;
@@ -30,11 +30,25 @@ function update(time) {
   updateCactus(delta, speedScale);
   updateSpeedScale(delta);
   updateScore(delta);
+  if (checkLose()) return handleLose();
 
   lastTime = time;
   window.requestAnimationFrame(update);
 }
-window.requestAnimationFrame(update);
+
+function checkLose() {
+  const dinoRect = getDinoRect();
+  return getCactusRects().some((rect) => isCollision(rect, dinoRect));
+}
+
+function isCollision(rect1, rect2) {
+  return (
+    rect1.left < rect2.right &&
+    rect1.top < rect2.bottom &&
+    rect1.right > rect2.left &&
+    rect1.bottom > rect2.top
+  );
+}
 
 function updateSpeedScale(delta) {
   speedScale += delta * SPEED_SCALE_INCREASE;
@@ -54,6 +68,14 @@ function handleStart() {
   setupCactus();
   startScreenElem.classList.add("hide");
   window.requestAnimationFrame(update);
+}
+
+function handleLose() {
+  setDinoLose();
+  setTimeout(() => {
+    document.addEventListener("keydown", handleStart, { once: true });
+    startScreenElem.classList.remove("hide");
+  }, 100);
 }
 
 function setPixelToWorldScale() {
