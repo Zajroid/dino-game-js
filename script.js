4d42d4bc63b0924fef5f6586ecafc5bbbae74a1a
Danya <zajroid@gmail.com>
2022-08-23 11:08:06 +0300

    base styles

---
+++ script.js
@@ -0,0 +1,9 @@
+const WORLD_WIDTH = 100;
+const WORLD_HEIGHT = 30;
+
+
+const worldElem = document.querySelector('[data-world]');
+
+setPixelToWorldScale();
+window.addEventListener('resize', setPixelToWorldScale);
+
